0774ebd30977f951ddf88d3348e572d91d7ec1fd
abuhanaan <adewunmi99@gmail.com>
2022-05-18 21:58:05 +0100

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.png'
+import './App.css';
+import {getFullYear, getFooterCopy} from './utils'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr/>
+      <div className='my-body'>
+        <body>
+          <p>Login to access the full dashboard</p>
+        </body>
+      </div>
+      <hr/>
+      <footer className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
